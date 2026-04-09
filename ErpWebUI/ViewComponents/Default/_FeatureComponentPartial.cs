269ec703caee72c92f836b6ee2ff2f7b09f804bd
enes-ozdemir23 <149109601+enes-ozdemir23@users.noreply.github.com>
2026-04-10 02:04:07 +0300

    # ViewComponent kullanılarak sayfa tasarımı yapıldı

---
+++ ErpWebUI/ViewComponents/Default/_FeatureComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErpWebUI.ViewComponents.Default
+{
+    public class _FeatureComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ ErpWebUI/ViewComponents/Default/_HeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErpWebUI.ViewComponents.Default
+{
+    public class _HeadComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
