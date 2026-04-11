03e6dd3917f270d3d0d98292a69ccc0f8664f505
enes-ozdemir23 <149109601+enes-ozdemir23@users.noreply.github.com>
2026-04-12 00:32:14 +0300

    #ViewComponentler ile tema düzenlendi

---
+++ ErpWebUI/ViewComponents/Default/_PopularProductComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErpWebUI.ViewComponents.Default
+{
+    public class _PopularProductComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ ErpWebUI/ViewComponents/Default/_WhyChooseUsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErpWebUI.ViewComponents.Default
+{
+    public class _WhyChooseUsComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
