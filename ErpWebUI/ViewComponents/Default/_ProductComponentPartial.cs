73fb1c23fdf1e64e93aae14ac4c569fd1873ac4d
enes-ozdemir23 <149109601+enes-ozdemir23@users.noreply.github.com>
2026-04-11 23:57:41 +0300

    #Default Sayfasının düzenlenmesi

---
+++ ErpWebUI/ViewComponents/Default/_ProductComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErpWebUI.ViewComponents.Default
+{
+    public class _ProductComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
