33cc56a6e79980f8c4e6082d9d015a74179d79c9
enes-ozdemir23 <149109601+enes-ozdemir23@users.noreply.github.com>
2026-04-13 01:36:24 +0300

    #Default sayfası viewcomponent kullanılarak tamamlandı

---
+++ ErpWebUI/ViewComponents/Default/_BestSellingProductsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErpWebUI.ViewComponents.Default
+{
+    public class _BestSellingProductsComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ ErpWebUI/ViewComponents/Default/_ScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErpWebUI.ViewComponents.Default
+{
+    public class _ScriptComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
