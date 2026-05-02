7e2f3898ed5610f8158707101209fbd73233e78b
enes-ozdemir23 <149109601+enes-ozdemir23@users.noreply.github.com>
2026-05-02 22:19:30 +0300

    #Kategori sayfasının verileri GroupCode consume yapılarak başarıyla getirildi.

--- ErpWebUI/Controllers/CategoryController.cs
+++ ErpWebUI/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ErpWebUI.Dtos.GroupCodeDtos;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace ErpWebUI.Controllers
@@ -15,11 +16,12 @@ namespace ErpWebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var client=_httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("");
+            var responseMessage = await client.GetAsync("https://localhost:7196/api/GroupCodes");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData=await responseMessage.Content.ReadAsStringAsync();
-                //var values=JsonConvert.DeserializeObject<List<>>(jsonData);
+                var values=JsonConvert.DeserializeObject<List<ResultGroupCodeDto>>(jsonData);
+                return View(values);
             }
             
             return View();
