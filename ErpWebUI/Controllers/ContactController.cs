35c9816d4e63b56768cdf3e3e4d3208126319602
enes-ozdemir23 <149109601+enes-ozdemir23@users.noreply.github.com>
2026-05-15 00:02:23 +0300

    #Default sayfasınıın contact consume işlemi gerçekleştirildi. Veriler veritabanına kaydediliyor ama status ve date alanları eklenmesi gerek.

--- ErpWebUI/Controllers/ContactController.cs
+++ ErpWebUI/Controllers/ContactController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ErpWebUI.Dtos.ContactDtos;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ErpWebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7196/api/Contacts", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                TempData["Success"] = "Mesajınız başarıyla gönderildi.";
+                return RedirectToAction("Index");
+            }
+            TempData["Error"] = "Mesaj gönderilemedi, lütfen tekrar deneyin.";
+            return View();
+        }
     }
 }
+
