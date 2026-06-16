31d9c5a2abc1fe0cd93c3a3ee2f145cef163e87a
enes-ozdemir23 <149109601+enes-ozdemir23@users.noreply.github.com>
2026-06-16 22:12:58 +0300

    #About Sayfasının verileri veritabanından çekilerek sayfaya yansıtıldı.

--- ErpWebUI/Controllers/AboutController.cs
+++ ErpWebUI/Controllers/AboutController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ErpWebUI.Dtos.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ErpWebUI.Controllers
 {
     public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AboutController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7196/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values.FirstOrDefault());
+            }
+
             return View();
         }
     }
